e97ed39c6e7f163d49e8380d79037ea9416f2e6a
nalrabah <nalrabah@hotmail.com>
2019-01-23 18:20:24 +0300

    Nourah and Fatma N

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=120)
+	description = models.CharField(max_length=120)
+	opening_time = models.TimeField(auto_now=False)
+	closing_time = models.TimeField(auto_now=False)
+
+	def __str__(self):
+		return self.name
